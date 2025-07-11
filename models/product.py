f7d3e8d4b76a30f3c24f8e16b9d43fb7f8f5c10c
Amirreza Arab shahrab <amrearsh@gmail.com>
2025-07-11 18:02:11 +0330

    finish class for database

---
+++ models/product.py
@@ -0,0 +1,50 @@
+from sqlalchemy import Column, Integer, String, Float
+from db.database import Base1,Base2,Base3,Base4,Base5
+
+#کالا
+class commodity(Base1):
+    __tablename__ = "commodites"
+
+    name = Column(String, index=True)
+    sku = Column(Integer)
+    price = Column(Integer)
+    tedad = Column(Integer)
+    min_threshold = Column(Integer)
+    subject = Column(String, index=True)
+
+#کاربران 
+class users(Base2):
+    __tablename__ = "users"
+
+    fname = Column(String, index=True)
+    lname = Column(String, index=True)
+    kname = Column(String, index=True , unique=True )
+    email = Column(String, index=True , unique=True )
+    password = Column(String)
+
+#تامین کننده
+class seller(Base3):
+    __tablename__ = "seller"
+
+    name = Column(String, index=True)
+    subject = Column(String, index=True)
+    tel = Column(Integer)
+    email = Column(String, index=True , unique=True)
+    point = Column(Integer)
+
+#گزارش
+class text(Base4):
+    __tablename__ = "texts"
+
+    subject = Column(String, index=True)
+    text = Column(String, index=True)
+
+#سفارش خرید
+class gets(Base5):
+    __tablename__ = "list"
+    
+    name = Column(String)
+    tedad = Column(Integer)
+    subject = Column(String)
+    name_seller = Column(String)
+    vaziat = Column(Integer)
--- schemas/product.py
+++ schemas/product.py
@@ -11,6 +11,8 @@ class commodityBase(BaseModel):
     subject: str = Field(min_length=1)
     #pic
 
+class commodity(commodityBase):
+
     class Config:
         orm_mode = True
 
@@ -19,8 +21,10 @@ class usersBase(BaseModel):
     fname: str = Field(min_length=1)
     lname: str = Field(min_length=1)
     kname: str = Field(min_length=1)
+    email: str = Field(min_length=1)
     password: str = Field(min_length=8)
 
+class users(usersBase):
 
     class Config:
         orm_mode = True
@@ -33,6 +37,8 @@ class sellerBase(BaseModel):
     email : str = Field(min_length=1)
     point: int = Field(gt=1, lt=10)
 
+class seller(sellerBase):
+
     class Config:
         orm_mode = True
 
@@ -42,16 +48,20 @@ class textBase(BaseModel):
     subject: str = Field(min_length=1)
     text: str = Field(min_length=1)
 
+class text(textBase):
+
     class Config:
         orm_mode = True
 
 #سفارش خرید
 class getsBase(BaseModel):
     name: str = Field(min_length=1)
-    price: int = Field(gt=0)
+    tedad: int = Field(gt=0)
     subject: str = Field(min_length=1)
     name_seller : str = Field(min_length=1)
     vaziat: int = Field(gt=0, lt=4)
+
+class gets(getsBase):
 
     class Config:
         orm_mode = True
